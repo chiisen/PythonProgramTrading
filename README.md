1042b370f3fe61b527cb9b9ce667543d0b518583
chiisen <chiisen@hotmail.com>
2023-12-13 03:36:55 +0800

    feat: Python 下載網頁內容 (request.py)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PythonProgramTrading
 Python 程式交易
+
+1. Python 下載網頁內容 (request.py)
+
---
+++ request.py
@@ -0,0 +1,5 @@
+import requests
+
+html=requests.get('https://tw.stock.yahoo.com/rank/change-up?exchange=TAI') # 取得網頁內容
+
+print(html.text) # 查看網頁內容
